a72df4655ceb7d873ef80669c72df7b14c2af0ed
Charlie Bi <charlie_bi@hotmail.com>
2025-06-12 19:50:07 -0400

    Update Powerpack

--- Assets/[Scripts]/PowerupPack/PowerupPack.cs
+++ Assets/[Scripts]/PowerupPack/PowerupPack.cs
@@ -29,8 +29,8 @@ public class PowerupPack : MonoBehaviour
 
     public void OpenPowerPack()
     {
-        if (GameManager.Instance.TrySetGameState(GameState.PAUSED) != GameState.PAUSED) return;
         if (PowerPackCount <= 0) return;
+        if (GameManager.Instance.TrySetGameState(GameState.PAUSED) != GameState.PAUSED) return;
         PowerPackCount--;
         PowerPackCountChanged?.Invoke(PowerPackCount);
         tmp.text = PowerPackCount.ToString();
